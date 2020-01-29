753a92ed3f08b37f0fc3aabdd2ef84a9f8c62139
revovich <revovich@gmail.com>
2020-01-29 18:01:17 +0300

    final part

--- scripts.js
+++ scripts.js
@@ -7,4 +7,9 @@ $(function() {
         $('.toggle').toggleClass('open');
         $('.nav-list').toggleClass('open');
     });
+
+    AOS.init({
+        easing: 'ease',
+        duration: 1000,
+    });
 });
